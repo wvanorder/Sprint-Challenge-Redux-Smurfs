90dac9124491602a3f5f77264a24073e08b0bb4b
VanOrder <vanorder@VanOrders-iMac.local>
2019-06-21 08:20:05 -0800

    got the get, post, put, and delete requests all working properly!

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -6,17 +6,130 @@ import './App.css';
  Just remember, `how do I `connect` my components to redux?`
  `How do I ensure that my component links the state to props?`
  */
+
+import { connect } from 'react-redux';
+
+import { loadSmurfs, addSmurf, deleteSmurf, updateSmurf } from '../actions';
+
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      newSmurf: {
+        name: '',
+        age: '',
+        height: '',
+      }
+    }
+  }
+
+  componentDidMount(){
+    this.props.loadSmurfs();
+  }
+
+  handleChange = e => {
+    e.preventDefault();
+    this.setState({
+      newSmurf:{
+        ...this.state.newSmurf,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
+  clearForm = () => {
+    this.setState({
+      newSmurf: {
+        name: '',
+        age: '',
+        height: '',
+      }
+    })
+  }
+
+  addSmurf = () => {
+    this.props.addSmurf(this.state.newSmurf);
+    this.setState({
+      newSmurf: {
+        name: '',
+        age: '',
+        height: ''
+      }
+    })
+  }
+
+  deleteSmurf = id => {
+    this.props.deleteSmurf(id)
+  }
+
+  updateSmurf = (smurf, id) => {
+    if(!this.state.newSmurf.name || !this.state.newSmurf.age || !this.state.newSmurf.height){
+      this.setState({
+        newSmurf:{
+          ...smurf
+        }
+      })
+    } else {
+      this.props.updateSmurf(this.state.newSmurf, id);
+    }
+  }
+
+
   render() {
     return (
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        {this.props.smurfs.map(smurf => {
+          return(
+            <div className='smurf-card'>
+              <h2>{smurf.name}</h2>
+              <h4>{smurf.age}</h4>
+              <h6>{smurf.height}cm  short</h6>
+              <button onClick={() => this.deleteSmurf(smurf.id)}>Smite {smurf.name}</button>
+              <button onClick={() => this.updateSmurf(smurf, smurf.id)}>Update {smurf.name}'s information</button>
+            </div>
+          )
+        })}
+        <form>
+          <input placeholder='smurf name'
+          onChange={this.handleChange}
+          value={this.state.newSmurf.name}
+          name="name"
+          required="fill me out"
+          />
+          <input placeholder='age'
+          onChange={this.handleChange}
+          value={this.state.newSmurf.age}
+          name="age"
+          required="fill me out"
+          />
+          <input placeholder='height'
+          onChange={this.handleChange}
+          value={this.state.newSmurf.height}
+          name="height"
+          required="fill me out"
+          />
+        </form>
+        <button onClick={this.addSmurf}>Add a New Smurf!</button>
+        <button onClick={this.clearForm}>Clear Form</button>
       </div>
     );
   }
 }
 
-export default App;
+App.defaultProps = {
+  smurfs: []
+};
+
+const mapStateToProps = state => {
+  return{
+    smurfs: state.smurfs,
+    fetchingSmurfs: state.fetchingSmurfs
+  }
+};
+
+export default connect(
+  mapStateToProps,
+  {loadSmurfs, addSmurf, deleteSmurf, updateSmurf }
+)(App);
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -21,3 +21,95 @@
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+
+import {
+  LOAD_SMURFS_START,
+  LOAD_SMURFS_SUCCESS,
+  LOAD_SMURFS_FAILURE,
+  ADD_SMURFS_START,
+  ADD_SMURFS_SUCCESS,
+  ADD_SMURFS_FAILURE,
+  DELETE_SMURF_START,
+  DELETE_SMURF_SUCCESS,
+  DELETE_SMURF_FAILURE,
+  UPDATE_SMURF_START,
+  UPDATE_SMURF_SUCCESS,
+  UPDATE_SMURF_FAILURE,
+} from '../actions';
+
+const initialState = {
+    smurfs: [],
+   fetchingSmurfs: false,
+   addingSmurf: false,
+   updatingSmurf: false,
+   deletingSmurf: false,
+   error: '',
+};
+
+const reducer = (state=initialState, action) => {
+  switch(action.type) {
+    case LOAD_SMURFS_START:
+    return{
+      ...state,
+      error: '',
+      fetchingSmurfs: true,
+    };
+    case LOAD_SMURFS_SUCCESS:
+    return{
+      ...state,
+      smurfs: action.payload,
+      error: '',
+      fetchingSmurfs: false,
+    };
+    case LOAD_SMURFS_FAILURE:
+    return{
+      ...state
+    };
+    case ADD_SMURFS_START:
+    return{
+      ...state,
+      addingSmurf: true,
+      error: '',
+    };
+    case ADD_SMURFS_SUCCESS:
+    return{
+      ...state,
+      smurfs: action.payload,
+      error: '',
+      addingSmurf: false,
+    };
+    case DELETE_SMURF_START:
+    return{
+      ...state,
+      fetchingSmurfs: true,
+      error: '',
+    };
+    case DELETE_SMURF_SUCCESS:
+    return{
+      ...state,
+      fetchingSmurfs: false,
+      smurfs: action.payload
+    };
+    case UPDATE_SMURF_START:
+    return{
+      ...state,
+      fetchingSmurfs: true,
+    };
+    case UPDATE_SMURF_SUCCESS:
+    return{
+      ...state,
+      smurfs: action.payload,
+      error: '',
+      fetchingSmurfs: false,
+    };
+    default:
+    return state;
+  }
+};
+
+export default reducer;
+
+
+
+
+
